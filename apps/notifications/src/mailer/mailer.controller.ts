305388210c7e1851285f4fe6df276c8f1d7eacb8
Alessio <alexeimv91@gmail.com>
2024-10-14 17:27:02 -0400

    config template

---
+++ apps/notifications/src/mailer/mailer.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+import { EventPattern } from '@nestjs/microservices';
+
+@Controller('mailer')
+export class MailerController {
+  @EventPattern('order_created')
+  async handleOrderCreated(data: any) {
+    console.log(data);
+  }
+}
